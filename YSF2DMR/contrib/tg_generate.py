f1347ef419266dff096f3158b052befba0401516
msraya <msraya@users.noreply.github.com>
2019-03-24 20:51:24 +0100

    Add files via upload
    Solved problems with unicode characters and some TG Names
    Preparing changes on YSF2DMR so we can show TG Names instead of TG Numbers

--- YSF2DMR/contrib/tg_generate.py
+++ YSF2DMR/contrib/tg_generate.py
@@ -1,3 +1,4 @@
+
 f=open('/tmp/group.txt')
 group=f.readlines()
 f.close()
@@ -19,13 +20,39 @@ content=[x.strip() for x in content]
 content = content[1:]
 content = content[:-1]
 f = open("/tmp/TGList.txt","wb+")
-
+first_time = True
 for line in content:
+  #Solve unicode string replacement
+  line=line.replace("\/"," ")
+  line=line.replace("\u00e9","e")
+  line=line.replace("\u00fa","u")
+  line=line.replace("\u00ed","i")
+  line=line.replace("\u00f4","o")
+  line=line.replace("\u00e4","a")
+  line=line.replace("\u00e1","a")
+  line=line.replace("\u00f3","o")
+  line=line.replace("\u00f6","o")
+  line=line.replace("\u00f1","n")
+  line=line.replace("\u2019"," ")
+  line=line.replace("\u00e8","e")
+  line=line.replace("\u00fc","u")
+  line=line.replace("Provincial","Prov.")
+  #delete line feed
+  line=line.replace("\n","")
+  line=line.replace("\r","")
+  #delete json simbols
   line=line.replace('"','')
   line=line.replace(',','')
   line=line.split(':')
   i=line[0]
   val=int(i)
+  # Include Spanish Multimode TG
+  if first_time and val>2140:
+    c=tg_count(2140) 
+    tmp="2140;0;" + str(c) + ";MULTIMODE SPAIN;MULTIMODE SPAIN"
+    f.write(tmp+'\n')
+    first_time = False
+  #process special TG
   if (val>=4000) and (val<=5000):
     b='1'
   else:
@@ -35,6 +62,7 @@ for line in content:
     c=tg_count(val)
   else:
     c=0
+  #write final lines
   line=line[0]+';'+ str(b)+';'+str(c)+';'+line[1]+';'+line[1]
   line=line.replace(' ','')
   f.write(line+'\n')
